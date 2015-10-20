af1de880b3e6b6a60d6ff6f563b4f02862dda348
husnain446 <husnain446@gmail.com>
2015-10-20 11:28:14 +0500

    add classes

---
+++ app/src/main/java/com/byteshaft/order_booker/OrderActivity.java
@@ -0,0 +1,14 @@
+package com.byteshaft.order_booker;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OrderActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_activity);
+    }
+}
